9c2bf0aba195f2e9e26e6e325d4e2794e037361b
Albania Musabeli <musabelialbania@gmail.com>
2023-12-30 15:41:23 -0300

    feat: Agregar sidebar y navbar de Angular Material

--- src/app/ty/ty.module.ts
+++ src/app/ty/ty.module.ts
@@ -6,6 +6,7 @@ import { CardTyComponent } from './components/card-ty/card-ty.component';
 import { BuscadorComponent } from './components/buscador/buscador.component';
 import { TyRoutingModule } from './ty-routing.module';
 import { MainComponent } from './pages/main/main.component';
+import { MaterialModule } from '../material/material.module';
 
 
 
@@ -20,7 +21,8 @@ import { MainComponent } from './pages/main/main.component';
   ],
   imports: [
     CommonModule,
-    TyRoutingModule
+    TyRoutingModule,
+    MaterialModule
   ]
 })
 export class TyModule { }
